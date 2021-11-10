1c11bb61d24d1f751a41606de6c6aeb56ebe7fd9
MrLevtastic <Levtastic@users.noreply.github.com>
2021-11-10 13:02:54 +0000

    Simplify tp_link_broadcast.py
    Remove java class and sources from comments
    Hardcode values that never change
    Remove unnecessary variables
    Change from java-style arrayCopy to python slicing
    Remove short functions that only get called once (or only wrap a python function)
    Remove check for null on string - pass an empty string for null-like behaviour

--- PyP100/tp_link_broadcast.py
+++ PyP100/tp_link_broadcast.py
@@ -3,153 +3,26 @@ import random
 from zlib import crc32
 
 
-# (sources) com\tplink\tdp\common\b.java (algorithm)
-# (sources) b\d\h\k.java:269 (calling algorithm)
-# (sources) com\tplink\tdp\common\c.java (values for calling)
-# (sources) b\d\f\e\a.java (int/short to byte conversion)
-
-# Original (relevant) code:
-
-# /* compiled from: TDPPacket */
-# public class b {
-#     private byte a;
-
-#     /* renamed from: b  reason: collision with root package name */
-#     private byte f10641b;
-
-#     /* renamed from: c  reason: collision with root package name */
-#     private short f10642c;
-
-#     /* renamed from: d  reason: collision with root package name */
-#     private short f10643d;
-
-#     /* renamed from: e  reason: collision with root package name */
-#     private byte f10644e;
-
-#     /* renamed from: f  reason: collision with root package name */
-#     private byte f10645f;
-#     private int g;
-#     private int h;
-#     protected byte[] i = new byte[0];
-
-#     public b(byte b2, short s, byte b3, String str) {
-#         byte[] bArr;
-#         this.a = b2;
-#         this.f10641b = 0;
-#         this.f10642c = s;
-#         if (str != null) {
-#             bArr = str.getBytes();
-#             this.f10643d = (short) bArr.length;
-#         } else {
-#             this.f10643d = 0;
-#             bArr = null;
-#         }
-#         this.f10644e = b3;
-#         this.f10645f = 0;
-#         this.g = c();
-#         this.h = 1516993677;
-#         this.i = new byte[(this.f10643d + 16)];
-#         b();
-#         if (bArr != null) {
-#             System.arraycopy(bArr, 0, this.i, 16, this.f10643d);
-#         }
-#         a();
-#     }
-
-#     private void a() {
-#         CRC32 crc32 = new CRC32();
-#         crc32.update(this.i);
-#         int value = (int) crc32.getValue();
-#         this.h = value;
-#         System.arraycopy(a.c(value), 0, this.i, 12, 4);
-#     }
-
-#     private void b() {
-#         byte[] bArr = this.i;
-#         byte[] bArr2 = {this.a};
-#         System.arraycopy(bArr2, 0, bArr, 0, 1);
-#         bArr2[0] = this.f10641b;
-#         System.arraycopy(bArr2, 0, bArr, 1, 1);
-#         System.arraycopy(a.d(this.f10642c), 0, bArr, 2, 2);
-#         byte[] d2 = a.d(this.f10643d);
-#         System.arraycopy(d2, 0, bArr, 4, 2);
-#         d2[0] = this.f10644e;
-#         System.arraycopy(d2, 0, bArr, 6, 1);
-#         d2[0] = this.f10645f;
-#         System.arraycopy(d2, 0, bArr, 7, 1);
-#         System.arraycopy(a.c(this.g), 0, bArr, 8, 4);
-#         System.arraycopy(a.c(1516993677), 0, bArr, 12, 4);
-#     }
-
-#     private int c() {
-#         return new Random().nextInt(268435456) + 0;
-#     }
-# }
-
-
-# Decompiled java converted (badly, by hand) to python
 class b:
-    def __init__(self, b2, s, b3, string):
-        bArr = None
-        self.a = b2
-        self.f10641b = 0
-        self.f10642c = s
+    def __init__(self, string):
+        self.string_length = len(string)
 
-        if string:
-            bArr = string.encode('utf-8')
-            self.f10643d = len(bArr)
-        else:
-            bArr = None
-            self.f10643d = 0
-
-        self.f10644e = b3
-        self.f10645f = 0
-
-        self.g = self.f_c()
-
-        self.h = 1516993677
-        self.i = bytearray(self.f10643d + 16)
+        self.i = bytearray(self.string_length + 16)
+        self.i[16:] = string.encode('utf-8')
 
         self.f_b()
 
-        if bArr:
-            self.arrayCopy(bArr, 0, self.i, 16, self.f10643d)
-
-        self.f_a()
-
-    @staticmethod
-    def arrayCopy(src, srcPos, dest, destPos, length):
-        dest[destPos:destPos + length] = src[srcPos:srcPos + length]
-
-    def f_a(self):
-        value = int(crc32(self.i) & 0xffffffff)
-        self.h = value
-        self.arrayCopy(self.f_ac(value), 0, self.i, 12, 4)
+        self.i[12:16] = crc32(self.i).to_bytes(4, 'big')
 
     def f_b(self):
-        bArr = self.i
-        bArr2 = [self.a]
-        self.arrayCopy(bArr2, 0, bArr, 0, 1)
-        bArr2[0] = self.f10641b
-        self.arrayCopy(bArr2, 0, bArr, 1, 1)
-        self.arrayCopy(self.f_ad(self.f10642c), 0, bArr, 2, 2)
-        d2 = self.f_ad(self.f10643d)
-        self.arrayCopy(d2, 0, bArr, 4, 2)
-        d2[0] = self.f10644e
-        self.arrayCopy(d2, 0, bArr, 6, 1)
-        d2[0] = self.f10645f
-        self.arrayCopy(d2, 0, bArr, 7, 1)
-        self.arrayCopy(self.f_ac(self.g), 0, bArr, 8, 4)
-        self.arrayCopy(self.f_ac(1516993677), 0, bArr, 12, 4)
-
-    def f_ac(self, value):
-        return bytearray(value.to_bytes(4, 'big'))
-
-    def f_ad(self, value):
-        return bytearray(value.to_bytes(2, 'big'))
-
-    def f_c(self):
-        return random.randrange(268435456)
+        self.i[0] = 2
+        self.i[1] = 0
+        self.i[2:4] = (1).to_bytes(2, 'big')
+        self.i[4:6] = self.string_length.to_bytes(2, 'big')
+        self.i[6] = 17
+        self.i[7] = 0
+        self.i[8:12] = random.randrange(268435456).to_bytes(4, 'big')
+        self.i[12:16] = (1516993677).to_bytes(4, 'big')
 
 
 if __name__ == '__main__':
@@ -182,8 +55,7 @@ if __name__ == '__main__':
         separators=(',', ':')
     )
 
-    # class always gets called with these parameters if doing a broadcast
-    newb = b(2, 1, 16 | 1, payload)
+    newb = b(payload)
 
     for result in broadcast(newb.i):
         print(result)
